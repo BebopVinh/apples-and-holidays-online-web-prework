6d021fef82eac245c4bf5591ea71bbfc95c0a759
Vinh Do <vinhdo07@gmail.com>
2019-01-19 04:12:13 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -79,6 +79,8 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  holiday_hash.each do |season, date|
+    season.collect do |holiday, items|
 
 end
 
