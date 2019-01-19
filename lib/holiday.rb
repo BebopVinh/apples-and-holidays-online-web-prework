e04fc12f98ffe37d105bc904a9926547bade2feb
Vinh Do <vinhdo07@gmail.com>
2019-01-19 04:15:53 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -80,7 +80,6 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season, date|
     season.collect do |holiday, items|
-
       puts "#{holiday.to_s.capitalize!} : #{items.join(",")}"
     end
   end
